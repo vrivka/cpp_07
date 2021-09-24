a7e3bbdd09ab50f2571afb7630dc2a973e3ef514
vrivka <vladkolezov@gmail.com>
2021-09-24 12:31:47 +0300

    ex01 add chevrons

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 #include "iter.hpp"
 
-//template<typename T>
-//void print(T const &a) {
-//	std::cout << a << std::endl;
-//}
-//
-//template<typename T>
-//void plus_three(T const &a) {
-//	const_cast<T &>(a) = a + 3;
-//}
-//
-//int main() {
-//	int arr[3] = {1, 2, 3};
-//
-//	iter(arr, 3, print);
-//	iter(arr, 3, plus_three);
-//	iter(arr, 3, print);
-//}
+template<typename T>
+void print(T const &a) {
+	std::cout << a << std::endl;
+}
 
-class Awesome
-{
-public:
-	Awesome( void ) : _n( 42 ) { return; }
-	int get( void ) const { return this->_n; }
-private:
-	int _n;
-};
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
-template< typename T >
-void print( T const & x ) { std::cout << x << std::endl; return; }
+template<typename T>
+void plus_three(T const &a) {
+	const_cast<T &>(a) = a + 3;
+}
+
 int main() {
-	int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
-	Awesome tab2[5];
-	iter( tab, 5, print );
-	iter( tab2, 5, print );
-	return 0;
+	int arr[3] = {1, 2, 3};
+
+	iter<int>(arr, 3, print);
+	iter<int>(arr, 3, plus_three);
+	iter<int>(arr, 3, print);
 }
