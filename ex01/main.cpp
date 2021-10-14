7d072b80bc7e17ee8520eca41c252278a943cf81
vrivka <vladkolezov@gmail.com>
2021-10-14 14:30:49 +0300

    edit main ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -17,18 +17,20 @@ int main() {
 		int arr[3] = {1, 2, 3};
 		int len = sizeof(arr) / sizeof(int);
 
+		std::cout << "Let's create array of ints, and print them:\n";
 		iter<int>(arr, len, print);
-		std::cout << std::endl;
+		std::cout << "Now to apply function that add 3 to all values\nPrint array of ints:\n";
 		iter<int>(arr, len, plus_space);
 		iter<int>(arr, len, print);
 	}
 	std::cout << "\n\n";
 	{
+		std::cout << "Let's create array of strings, and print them:\n";
 		std::string arr[5] = {"Hi", "Hello", "Privet", "Bonjour", "Konnichiwa"};
 		int len = sizeof(arr) / sizeof(std::string);
 
 		iter<std::string>(arr, len, print);
-		std::cout << std::endl;
+		std::cout << "Now to apply function that add ' ', to all, and then add \"World!\" at the end\nPrint array of strings:\n";
 		iter<std::string>(arr, len, plus_space);
 		iter<std::string>(arr, len, plus_world);
 		iter<std::string>(arr, len, print);
